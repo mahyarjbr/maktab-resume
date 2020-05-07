dbb21f9d4892dac55e973d8fa82ce4012db603d5
mahyarJbr <mahyarjbr1374@gmail.com>
2020-05-07 13:00:00 +0430

    Updates

---
+++ precache-manifest.8e7b72f302b89b7bbbf8db9e0a0da9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8ba26a6ae0535aa73326e313c2fb6c",
+    "url": "/maktab-resume/index.html"
+  },
+  {
+    "revision": "8ed7070d4d2ac2116c78",
+    "url": "/maktab-resume/static/css/main.03851182.chunk.css"
+  },
+  {
+    "revision": "0d289aac560e96a5c7af",
+    "url": "/maktab-resume/static/js/2.8211684c.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/maktab-resume/static/js/2.8211684c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed7070d4d2ac2116c78",
+    "url": "/maktab-resume/static/js/main.183e1ef3.chunk.js"
+  },
+  {
+    "revision": "1fa1e12f3784fd3e3603",
+    "url": "/maktab-resume/static/js/runtime-main.6861e6f1.js"
+  }
+]);
